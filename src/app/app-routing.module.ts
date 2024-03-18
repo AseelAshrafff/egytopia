a6d9104cf5eca142c20e5d318b950cef94e94bc9
AseelAshrafff <aseelashraf75@gmail.com>
2024-03-18 03:00:28 +0200

    create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/sign/login/login.component';
+import { RegisterComponent } from './page/sign/register/register.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HotelsComponent } from './page/hotels/hotels/hotels.component';
+import { HoteldetailsComponent } from './page/hotels/hoteldetails/hoteldetails.component';
 
 
 const routes: Routes = [
+  {path:'', redirectTo:'home'},
   {path:'home', component:HomeComponent},
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
   {path:'navbar', component:NavbarComponent},
+  {path:'hotels/:id', component:HotelsComponent},
+  {path:'hoteldetails/:id', component:HoteldetailsComponent},
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+
 import { HotelsComponent } from './page/hotels/hotels/hotels.component';
 import { HoteldetailsComponent } from './page/hotels/hoteldetails/hoteldetails.component';
+import { LoginComponent } from './page/sign/login/login.component';
+import { RegisterComponent } from './page/sign/register/register.component';
+import { HomeComponent } from './page/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     LoginComponent,
     RegisterComponent,
-    NavbarComponent,
-    FooterComponent,
     HomeComponent,
     HotelsComponent,
     HoteldetailsComponent
