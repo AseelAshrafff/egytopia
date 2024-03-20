4c5bd79ede155bb6c50533aaa7eb74f9497c2164
AseelAshrafff <aseelashraf75@gmail.com>
2024-03-20 02:21:57 +0200

    adding nav bar and filtering bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,28 @@ import { RegisterComponent } from './page/sign/register/register.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { HotelsComponent } from './page/hotels/hotels/hotels.component';
 import { HoteldetailsComponent } from './page/hotels/hoteldetails/hoteldetails.component';
+import { NotfoundComponent } from './page/notfound/notfound.component';
+import { ExperienceComponent } from './page/experiencepages/experience/experience.component';
+import { FreelanceComponent } from './page/freelancepages/freelance/freelance.component';
+import { FavoritesComponent } from './page/user/favorites/favorites.component';
+import { NotificationsComponent } from './page/user/notifications/notifications.component';
+import { ProfileComponent } from './page/user/profile/profile.component';
 
 
 const routes: Routes = [
-  {path:'', redirectTo:'home'},
+  {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'home', component:HomeComponent},
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
   {path:'navbar', component:NavbarComponent},
   {path:'hotels/:id', component:HotelsComponent},
   {path:'hoteldetails/:id', component:HoteldetailsComponent},
+  {path:'experience', component:ExperienceComponent},
+  {path:'freelance', component:FreelanceComponent},
+  {path:'favorite', component:FavoritesComponent},
+  {path:'notification', component:NotificationsComponent},
+  {path:'profile', component:ProfileComponent},
+  {path:'**', component:NotfoundComponent}
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,15 @@ import { LoginComponent } from './page/sign/login/login.component';
 import { RegisterComponent } from './page/sign/register/register.component';
 import { HomeComponent } from './page/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { NotfoundComponent } from './page/notfound/notfound.component';
+import { FreelanceComponent } from './page/freelancepages/freelance/freelance.component';
+import { ExperienceComponent } from './page/experiencepages/experience/experience.component';
+import { FavoritesComponent } from './page/user/favorites/favorites.component';
+import { ProfileComponent } from './page/user/profile/profile.component';
+import { NotificationsComponent } from './page/user/notifications/notifications.component';
+import { FiltersComponent } from './page/home/filters/filters.component';
+import { CardsComponent } from './pag/home/cards/cards.component';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +28,15 @@ import { NavbarComponent } from './component/navbar/navbar.component';
     RegisterComponent,
     HomeComponent,
     HotelsComponent,
-    HoteldetailsComponent
+    HoteldetailsComponent,
+    NotfoundComponent,
+    FreelanceComponent,
+    ExperienceComponent,
+    FavoritesComponent,
+    ProfileComponent,
+    NotificationsComponent,
+    FiltersComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
