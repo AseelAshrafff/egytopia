68bb2953971b118ba1800e489145183069c896ba
OmarrAhmedd <omarrahmedredaa@gmail.com>
2024-03-26 23:03:15 +0200

    "Login form with Validtion"

--- src/app/page/sign/login/login.component.ts
+++ src/app/page/sign/login/login.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor( ) {}
+
+  registerForm: FormGroup = new FormGroup({
+   
+    email: new FormControl(null, [Validators.required, Validators.email]),
+   
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+   
+  });
+  
+
+  
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+    // Process the form data further, like sending it to the server
+    console.log(this.registerForm.value);
+  }
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  ngOnInit(): void {
+   
+  }
 
 }
