da2f64d6c5cbd009f00826a71e04486beade6c91
Yasmeen Yasser <155325517+YASMIENYASSER@users.noreply.github.com>
2024-04-04 07:17:53 +0200

    .hintrc

--- src/app/page/selectedplace/hotels-cards/hotels-cards.component.ts
+++ src/app/page/selectedplace/hotels-cards/hotels-cards.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hotels-cards',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './hotels-cards.component.css'
 })
 export class HotelsCardsComponent {
+  constructor(private router: Router) {}
+  navigateToHotelsDetails() {
+    this.router.navigate(['/hoteldetails']);
+  }
+
 
 }
